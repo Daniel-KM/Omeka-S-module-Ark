482b5d8f7e683158643cac0307b3ff9c192be7f6
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-06-22 00:00:00 +0000

    Fixed upgrade for 3.5.7.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -24,8 +24,8 @@ if (version_compare($oldVersion, '3.5.7', '<')) {
     $settings->delete('ark_use_admin');
     $settings->delete('ark_use_public');
 
-    $settings->set('ark_name_noid_template', $settings->get('ark_name_noid_template'));
-    $settings->delete('ark_name_noid_template');
+    $settings->set('ark_name_noid_template', $settings->get('ark_noid_template'));
+    $settings->delete('ark_noid_template');
 
     $settings->set('ark_name', 'noid');
     $settings->set('ark_qualifier', 'internal');
