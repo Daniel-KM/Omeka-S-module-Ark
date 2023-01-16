f2bbc0815d8efabd884ac2454d3223fd2f626104
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-16 00:00:00 +0000

    Fixed form.

--- src/Service/Form/ConfigFormFactory.php
+++ src/Service/Form/ConfigFormFactory.php
@@ -10,7 +10,7 @@ class ConfigFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $configForm = new ConfigForm(null, $options);
+        $configForm = new ConfigForm(null, $options ?? []);
         return $configForm
             ->setArkManager($services->get('Ark\ArkManager'));
     }
